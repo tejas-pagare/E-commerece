a679cc0fc3488af49d6310b8cbe6492bb567745a
tejas-pagare <147067782+tejas-pagare@users.noreply.github.com>
2025-03-14 01:35:30 +0530

    models created for user and products

---
+++ models/product.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    rate: {
+      type: Number,
+      required: true,
+    },
+    count: {
+      type: Number,
+      required: true,
+    },
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    immutable: true, // Ensures createdAt can't be modified
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Middleware to update 'updatedAt' before saving
+productSchema.pre('save', function (next) {
+  this.updatedAt = Date.now();
+  next();
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
---
+++ models/user.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  profilePhoto: {
+    type: String,
+  },
+  address: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  cart: [
+    {
+      productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        default: 1,
+      },
+    },
+  ],
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
