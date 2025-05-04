33091c477eb78dd247ae3775d506a1aa9c1d7257
CanItBeCoded63 <yashsinghchouhan098@gmail.com>
2025-05-04 15:17:49 +0530

    user part updated

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const dbConnection = async()=>{
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect(process.env.MONGODB_URL||"mongodb+srv://tejaspagare1625:ffsdproject@cluster0.ha0yl.mongodb.net/");
     console.log("Connected to MongoDb")
   } catch (error) {
     console.log(error)
