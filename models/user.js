f7aaf1171adafe60891282f510c9d3511fb0ded7
RuchithPraharshaBandaru <ruchithpraharshabandaru@gmail.com>
2025-12-03 19:29:20 +0530

    added size functionality

--- models/user.js
+++ models/user.js
@@ -34,6 +34,10 @@ const userSchema = new mongoose.Schema({
       quantity: {
         type: Number,
         default: 0
+      },
+      size: {
+        type: String,
+        required: false 
       }
     }
   ],
